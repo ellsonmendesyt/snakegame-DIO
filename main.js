7605e268e5e0fa0abaf9cfde48884cd627b5a04a
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-11-16 17:48:47 -0300

    snake going all directions

--- main.js
+++ main.js
@@ -10,6 +10,16 @@ snake[0]={
     x: 8 * box,
     y: 8 * box,
 }
+let counter = 1;
+
+const left  =   37;
+const up    =   38;
+const right =   39;
+const down  =   40;
+
+
+let direction = 'right';
+document.addEventListener('keydown', update);
 
 function createBG(){
     context.fillStyle='lightgreen';
@@ -26,13 +36,59 @@ function createSnake(){
 }
 
 
-createBG();
-createSnake();
 
 
+///se a direção anteriro nao for a contraria
+function update(e){
+  if(e.keyCode == left && direction != 'right'){
+       direction='left' ;
+  }
+  if(e.keyCode==up && direction != 'down'){
+      direction = 'up'
+  }
+  if(e.keyCode== right && direction != 'left'){
+      direction='right';
+  }
+  if(e.keyCode == down && direction != 'up'){
+      direction= 'down';
+  }
+}
 
 
+//game loop
+function GameLoop(){
+   
+// LOGIC
+if(snake[0].x > (15 * box) && direction == 'right') snake[0].x=0;
+if(snake[0].x< 0 && direction == 'left') snake[0].x = box* 16;
+if(snake[0].y< 0 && direction == 'up') snake[0].y = box* 16;
+if(snake[0].y> box * 16 && direction == 'down') snake[0].y = 0;
 
+    createBG();
+    createSnake();
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    if(direction =='right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
+
+    snake.pop(); // take out a tail
+
+    //put a ne head
+    let newHead = {
+        x:snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+
+}
+
+
+let game = setInterval(GameLoop,100);
 
 
 
